12d433a4d00fbb6c0d96a0e6cc974ef6ef818f9c
adeoye adela <adeoye.adela@gmail.com>
2019-09-16 03:07:01 +0100

    Calculator now styled

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const NumberButton = props => {
   return (
-    <div>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className=".special_pro">{props.numbers}</button>
-    </div>
+  
+      <button className="special_pro " onClick={()=>'call a function' } >{props.numbers}</button>
+    
+  
   );
 };
 export default NumberButton;
